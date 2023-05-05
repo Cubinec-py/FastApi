3ad4b236ba99f7b74158aa13a91595f72b109d4b
Cubinec-py <san4os2012@gmail.com>
2023-05-05 21:33:35 +0300

    Using fastapi cache, celery tasks with flower as tasks dashboard & FastAPI BackgroundTasks

--- src/config.py
+++ src/config.py
@@ -4,11 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Database configs
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
+# Auth and Manager secrets
 SECRET_AUTH = os.environ.get("AUTH")
 SECRET_MANAGER = os.environ.get("MANAGER")
+
+# Mailing configs
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
---
+++ src/tasks/router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from starlette.background import BackgroundTasks
+
+from auth.base_config import current_user
+from tasks import send_mail
+
+
+router = APIRouter(
+    prefix='/api/tasks',
+    tags=['Tasks'],
+)
+
+
+@router.get("/dashboard")
+def get_dashboard_report(background_tasks: BackgroundTasks, user=Depends(current_user)):
+    # Simple complete - 1400ms
+    # send_mail(user.username)
+
+    # Using FastAPI BackgroundTasks in event loops - 500ms
+    # background_tasks.add_task(send_mail, user.username)
+
+    # Using Celery tasks - 600ms
+    send_mail.delay(user.username)
+    return {
+        "status": 200,
+        "data": "Письмо отправлено",
+        "details": None
+    }
