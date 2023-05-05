5cc4d41a880b99d3aef1693c3032ccceb7b31645
Cubinec-py <san4os2012@gmail.com>
2023-05-05 22:46:42 +0300

    Adding CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
+from fastapi.middleware.cors import CORSMiddleware
 
 from redis import asyncio as aioredis
 
@@ -12,6 +13,19 @@ from operations.router import router as router_operation
 
 app = FastAPI(
     title="Trading App"
+)
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 app.include_router(
